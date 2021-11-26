26ee438538c785e23452bfd03b75278947977e09
Max <gorodtira51@gmail.com>
2021-11-26 18:41:30 +0200

    working on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Link, Route } from 'react-router-dom';
 
 import './App.scss';
-import { UserInfo } from './components/UserInfo';
+import { UserInfo } from './components/UserInfo/UserInfo';
 import { UserList } from './components/UserList/UserList';
 
 export const App: React.FC = () => {
+  const [userId, setUserId] = useState(0);
+
+  const handleClick = (id: number) => {
+    setUserId(id);
+  }
 
   return (
   <div>
     <h1>
-      <Link to="/user">
-        UserInfo
-      </Link>
-      <Link to="/">
+
+      <Link to="">
         Home
       </Link>
     </h1>
     
     <Routes>
-      <Route path="/" element={<UserList />} />
-      <Route path="/user" element={<UserInfo />} />
+      <Route path="" element={
+      <UserList
+        handleClick={handleClick}
+      />
+      } />
+      <Route path={`/user/${userId}`} element={<UserInfo />} />
     </Routes>
   </div>
   );
