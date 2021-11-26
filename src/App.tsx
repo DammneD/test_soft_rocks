c8cdb35d4b5a006b61c7f0fea52d530dc97ee756
Max <gorodtira51@gmail.com>
2021-11-26 12:14:29 +0200

    create repo

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+import './App.scss';
+
+export const App = () => (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This works
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
